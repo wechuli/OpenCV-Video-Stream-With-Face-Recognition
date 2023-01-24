0da96e12f87381b04d3c7a4c5109c9f955c24cd3
Wechuli <wechulipaul@yahoo.com>
2023-01-24 21:16:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0',port=5050, debug=False)
